a792787e693541c870d437dbae6a906fe24618b8
kch9253 <104603779+kch9253@users.noreply.github.com>
2022-05-02 15:37:41 +0900

    Add files via upload

---
+++ trn01/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        vertical: true,
+        dots: true,
+    });
+})
