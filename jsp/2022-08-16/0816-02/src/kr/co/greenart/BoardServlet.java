75b99760d89e75d611482bf46eb2c661e3a35d64
kch9253 <104603779+kch9253@users.noreply.github.com>
2022-08-17 11:25:44 +0900

    Add files via upload

--- jsp/2022-08-16/0816-02/src/kr/co/greenart/BoardServlet.java
+++ jsp/2022-08-16/0816-02/src/kr/co/greenart/BoardServlet.java
@@ -10,13 +10,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebServlet("/board")
 public class BoardServlet extends HttpServlet {
+	private final static Logger logger = LoggerFactory.getLogger(BoardServlet.class);
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("사용자가 GET방식의 요청을 하였습니다. (게시판 목록)");
+//		System.out.println("사용자가 GET방식의 요청을 하였습니다. (게시판 목록)");
+		logger.info("logger를 통해 메세지를 남깁니다.");
+		logger.info("사용자가 Get 요청함. (게시판 목록)");
+		// slf4j - FATAL / ERROR / WARN / INFO / DEBUG
 		
 		List<String> articles = new ArrayList<>(Arrays.asList("글1", "글2", "글3"));
 		req.setAttribute("articles", articles);
@@ -24,3 +31,9 @@ public class BoardServlet extends HttpServlet {
 		req.getRequestDispatcher("/WEB-INF/articles.jsp").forward(req, resp);
 	}
 }
+
+
+
+
+
+
